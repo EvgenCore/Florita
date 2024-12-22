41d5ee1a587e449c21ad0e2c17c269b662b7b733
EvgenCore <johnshin378@gmail.com>
2024-12-22 22:57:30 +0100

    Languages buttons now works

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@ const translations = {
       navMenu: "MENU",
       navContact: "CONTACTS",
       
-      about_first_text_title: "Lorem ipsum dolor sit amet",
-      about_first_text_body: "Morbi a semper lacus. Pellentesque sit amet mollis mauris, sit amet commodo elit. Aliquam vestibulum velit id velit varius, vel pharetra justo tempor. Vivamus iaculis nisi in rutrum efficitur. Nam et dapibus velit. Donec a nunc aliquet, congue ipsum id, feugiat magna. Sed non interdum eros.",
-      about_second_text_title: "Lorem ipsum dolor sit amet",
-      about_second_text_body: "Morbi a semper lacus. Pellentesque sit amet mollis mauris, sit amet commodo elit. Aliquam vestibulum velit id velit varius, vel pharetra justo tempor. Vivamus iaculis nisi in rutrum efficitur. Nam et dapibus velit. Donec a nunc aliquet, congue ipsum id, feugiat magna. Sed non interdum eros.",
+      about_first_text_title: "The taste is impeccable!",
+      about_first_text_body: "We proudly offer you not only a variety of delicious dishes but also a unique experience that begins with the freshest ingredients and ends with masterfully prepared dishes that can satisfy even the highest gastronomic demands.",
+      about_second_text_title: "Refined style!",
+      about_second_text_body: "The interior of our restaurant is a blend of elegance and comfort. Every element of the decor has been chosen with love and attention to detail, creating an atmosphere where you’ll want to spend time with pleasure. Warm colors, stylish furniture, and soft lighting create the perfect setting for pleasant meetings and refined dinners.",
       menuTitle: "MENU",
-      socialTitle: "Lorem ipsum dolor sit ametvel pharetra justo tempor",
+      socialTitle: "Contact us in any way that is convenient for you",
       instaTikTokContainerTitle: "Our social networks"
     },
     es:{
@@ -20,6 +20,14 @@ const translations = {
         navAbout: "NOSOTROS",
         navMenu: "CARTA",
         navContact: "CONTACTOS",
+
+        about_first_text_title: "¡El sabor es impecable!",
+        about_first_text_body: "Con orgullo les ofrecemos no solo una variedad de platos deliciosos, sino también una experiencia única que comienza con los ingredientes más frescos y termina con platos magistralmente preparados, capaces de satisfacer incluso los más altos requisitos gastronómicos.",
+        about_second_text_title: "Estilo refinado!",
+        about_second_text_body: "El interior de nuestro restaurante es una combinación de elegancia y comodidad. Cada elemento de la decoración ha sido elegido con amor y atención al detalle, creando un ambiente en el que apetece pasar el tiempo con placer. Los colores cálidos, los muebles elegantes y la iluminación suave crean el ambiente perfecto para encuentros agradables y cenas refinadas.",
+        menuTitle: "CARTA",
+        socialTitle: "Contáctenos de la manera que le sea más conveniente",
+        instaTikTokContainerTitle: "Nuestras redes sociales"
         
     },
     ca:{
@@ -28,14 +36,30 @@ const translations = {
         navAbout: "NOSALTRES",
         navMenu: "CARTA",
         navContact: "CONTACTES",
+
+        about_first_text_title: "El gust és impecable!",
+        about_first_text_body: "Amb orgullus us oferim no només una varietat de plats deliciosos, sinó també una experiència única que comença amb els ingredients més frescos i acaba amb plats magistralment preparats que poden satisfer fins i tot els requisits gastronòmics més alts.",
+        about_second_text_title: "Estil refinat!",
+        about_second_text_body: "L'interior del nostre restaurant és una combinació d'elegància i comoditat. Cada element de la decoració ha estat triat amb amor i atenció als detalls, creant una atmosfera on es vol passar temps amb plaer. Els colors càlids, els mobles elegants i la il·luminació suau creen l'entorn perfecte per a trobades agradables i sopars refinats.",
+        menuTitle: "CARTA",
+        socialTitle: "Poseu-vos en contacte amb nosaltres de la manera que us sigui més còmoda",
+        instaTikTokContainerTitle: "Les nostres xarxes socials"
         
     },
     ua:{
         reserve: "РЕЗЕРВ",
-        navInicio: "ПОЧАТОК",
+        navInicio: "СТАРТ",
         navAbout: "ПРО НАС",
         navMenu: "МЕНЮ",
         navContact: "КОНТАКТИ",
+
+        about_first_text_title: "Смак бездоганний!",
+        about_first_text_body: "Ми з гордістю пропонуємо вам не лише різноманітні та смачні страви, але й унікальний досвід, який починається з найсвіжіших інгредієнтів і завершується майстерно приготованими стравами, здатними задовольнити навіть найвищі гастрономічні вимоги.",
+        about_second_text_title: "Вишуканий стиль!",
+        about_second_text_body: "Інтер'єр нашого ресторану — це поєднання елегантності та затишку. Кожен елемент декору був вибраний з любов'ю та увагою до деталей, створюючи атмосферу, в якій хочеться провести час із задоволенням. Теплі кольори, стильні меблі та м'яке освітлення створюють ідеальну обстановку для приємних зустрічей і вишуканих вечерь.",
+        menuTitle: "МЕНЮ",
+        socialTitle: "Зв'яжіться з нами будь-яким зручним способом",
+        instaTikTokContainerTitle: "Наші соціальні мережі"
        
     },
     ru: {
@@ -44,6 +68,14 @@ const translations = {
         navAbout: "О НАС",
         navMenu: "МЕНЮ",
         navContact: "КОНТАКТЫ",
+
+        about_first_text_title: "Вкус безупречен!",
+        about_first_text_body: "Мы с гордостью предлагаем вам не только разнообразные и вкусные блюда, но и уникальный опыт, который начинается с самых свежих ингредиентов и завершается мастерски приготовленными блюдами, способными удовлетворить даже самые высокие гастрономические требования.",
+        about_second_text_title: "Изысканный стиль!",
+        about_second_text_body: "Интерьер нашего ресторана — это сочетание элегантности и уюта. Каждый элемент декора был выбран с любовью и вниманием к деталям, создавая атмосферу, в которой хочется провести время с удовольствием. Теплые цвета, стильная мебель и мягкое освещение создают идеальную обстановку для приятных встреч и изысканных ужинов.",
+        menuTitle: "МЕНЮ",
+        socialTitle: "Свяжитесь с нами любым удобным способом",
+        instaTikTokContainerTitle: "Наши социальные сети"
        
     },
 
@@ -67,13 +99,17 @@ function changeLanguage(language) {
     const navContact = document.getElementById("navContact");
     const smallNavContacts = document.getElementById("smallNavContacts");
 
-   
 
-   
-    
-   
+    const about_first_text_title = document.getElementById("about_first_text_title");
+    const about_first_text_body = document.getElementById("about_first_text_body");
+    const about_second_text_title = document.getElementById("about_second_text_title");
+    const about_second_text_body = document.getElementById("about_second_text_body");
+    const menuTitle = document.getElementById("menuTitle");
+    const socialTitle = document.getElementById("socialTitle");
+    const instaTikTokContainerTitle = document.getElementById("instaTikTokContainerTitle");
 
-   
+
+
     reserveBtn.textContent = translations[language].reserve;
     navReserva.textContent = translations[language].reserve;
 
@@ -89,7 +125,14 @@ function changeLanguage(language) {
     navContact.textContent = translations[language].navContact;
     smallNavContacts.textContent = translations[language].navContact;
 
-    
+    about_first_text_title.textContent = translations[language].about_first_text_title;
+    about_first_text_body.textContent = translations[language].about_first_text_body;
+    about_second_text_title.textContent = translations[language].about_second_text_title;
+    about_second_text_body.textContent = translations[language].about_second_text_body;
+    menuTitle.textContent = translations[language].menuTitle;
+    socialTitle.textContent = translations[language].socialTitle;
+    instaTikTokContainerTitle.textContent = translations[language].instaTikTokContainerTitle;
+
    
 }
 
